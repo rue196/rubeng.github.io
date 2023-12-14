b82db766602e8f12177353fe450aa290b5467b05
rue196 <142844326+rue196@users.noreply.github.com>
2023-12-13 16:14:24 -0800

    Add files via upload

--- inti.js
+++ inti.js
@@ -2,9 +2,3 @@
   var dropdown = document.getElementById("myDropdown");
   dropdown.style.display = (dropdown.style.display === 'block') ? 'none' : 'block';
 }
-
-$(index.html).ready(function () {
-  if (!$.browser.webkit) {
-      $('.wrapper').html('<p>Sorry! Non webkit users. :(</p>');
-  }
-});
